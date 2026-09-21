02e8a1633e683e0f09c383fd8af1df8249c7db5a
ibag <ibagdesignengineer@gmail.com>
2026-09-21 11:11:53 +0100

    Generate Html for the Order Summary UI

--- Mofe-commerce/src/Pages/CheckOutPage.jsx
+++ Mofe-commerce/src/Pages/CheckOutPage.jsx
@@ -8,6 +8,8 @@ import "./CheckOutPage.css";
 export function CheckOutPage({ cart }) {
   const [deliveryOption, setDeliveryOption] = useState([]);
 
+  const [paymentSummary, setPaymentSummary] = useState(null);
+
   useEffect(() => {
     axios
       .get(
@@ -16,6 +18,11 @@ export function CheckOutPage({ cart }) {
       .then((response) => {
         setDeliveryOption(response.data);
       });
+
+    axios.get("http://localhost:3000/api/payment-summary").then((response) => {
+      setPaymentSummary(response.data);
+      alert(paymentSummary.totalItems);
+    });
   }, []);
 
   return (
@@ -38,84 +45,85 @@ export function CheckOutPage({ cart }) {
 
       <div className="main-body-content">
         <div className="all-product">
-          {deliveryOption.length > 0 && cart.map((cartItem) => {
+          {deliveryOption.length > 0 &&
+            cart.map((cartItem) => {
+              const selectedDeliveryOption = deliveryOption.find(
+                (deliverOption) => {
+                  return deliverOption.id === cartItem.deliveryOptionId;
+                },
+              );
 
-            const selectedDeliveryOption = deliveryOption.find(
-              (deliverOption) => {
-                return deliverOption.id === cartItem.deliveryOptionId;
-              },
-            );
-
-            return (
-              <div key={cartItem.productId} className="product-image-cotent">
-                <div className="product-image">
-                  <img
-                    src={`http://localhost:3000/${cartItem.product.image}`}
-                    //   "images/product/socks.jpg"
-                    alt=""
-                    className="product-image"
-                  />
-                </div>
-
-                <div className="all-content">
-                  <div className="delivery-price-quantity">
-                    <p className="tender-date">
-                      Delivery date:{" "}
-                      {dayjs(
-                        selectedDeliveryOption.estimatedDeliveryTimeMs,
-                      ).format("dddd, MMMM D")}
-                    </p>
-
-                    <h5>{cartItem.product.name}</h5>
-
-                    <h1>{formatCurrency(cartItem.product.priceCents)}</h1>
-
-                    <p>
-                      Quantity <span>{cartItem.quantity}</span>
-                    </p>
+              return (
+                <div key={cartItem.productId} className="product-image-cotent">
+                  <div className="product-image">
+                    <img
+                      src={`http://localhost:3000/${cartItem.product.image}`}
+                      //   "images/product/socks.jpg"
+                      alt=""
+                      className="product-image"
+                    />
                   </div>
 
-                  <div className="cancel-delivery-option">
-                    <div className="cancel-icon">
-                      <i className="hgi hgi-stroke hgi-rounded hgi-cancel-01"></i>
+                  <div className="all-content">
+                    <div className="delivery-price-quantity">
+                      <p className="tender-date">
+                        Delivery date:{" "}
+                        {dayjs(
+                          selectedDeliveryOption.estimatedDeliveryTimeMs,
+                        ).format("dddd, MMMM D")}
+                      </p>
+
+                      <h5>{cartItem.product.name}</h5>
+
+                      <h1>{formatCurrency(cartItem.product.priceCents)}</h1>
+
+                      <p>
+                        Quantity <span>{cartItem.quantity}</span>
+                      </p>
                     </div>
 
-                    <div className="delivery-option-and-header">
-                      <p>Choose Delivery Option:</p>
+                    <div className="cancel-delivery-option">
+                      <div className="cancel-icon">
+                        <i className="hgi hgi-stroke hgi-rounded hgi-cancel-01"></i>
+                      </div>
 
-                      <div className="delivery-options">
-                        {deliveryOption.map((deliverOption) => {
-                          let priceString = "Free Shipping";
+                      <div className="delivery-option-and-header">
+                        <p>Choose Delivery Option:</p>
 
-                          if (deliverOption.priceCents > 0) {
-                            priceString = `${formatCurrency(deliverOption.priceCents)} - Shipping `;
-                          }
-                          return (
-                            <label
-                              key={deliverOption.id}
-                              className="delivery-option"
-                            >
-                              <input
-                                type="radio"
-                                name={`delivery-${cartItem.productId}`}
-                                checked={
-                                  deliverOption.id === cartItem.deliveryOptionId
-                                }
-                              />
+                        <div className="delivery-options">
+                          {deliveryOption.map((deliverOption) => {
+                            let priceString = "Free Shipping";
 
-                              <div className="delivery-info">
-                                <h5>
-                                  {dayjs(
-                                    deliverOption.estimatedDeliveryTimeMs,
-                                  ).format("dddd, MMMM D")}
-                                </h5>
-                                <p>{priceString}</p>
-                              </div>
-                            </label>
-                          );
-                        })}
+                            if (deliverOption.priceCents > 0) {
+                              priceString = `${formatCurrency(deliverOption.priceCents)} - Shipping `;
+                            }
+                            return (
+                              <label
+                                key={deliverOption.id}
+                                className="delivery-option"
+                              >
+                                <input
+                                  type="radio"
+                                  name={`delivery-${cartItem.productId}`}
+                                  checked={
+                                    deliverOption.id ===
+                                    cartItem.deliveryOptionId
+                                  }
+                                />
 
-                        {/* <label className="delivery-option">
+                                <div className="delivery-info">
+                                  <h5>
+                                    {dayjs(
+                                      deliverOption.estimatedDeliveryTimeMs,
+                                    ).format("dddd, MMMM D")}
+                                  </h5>
+                                  <p>{priceString}</p>
+                                </div>
+                              </label>
+                            );
+                          })}
+
+                          {/* <label className="delivery-option">
                           <input type="radio" name="delivery" checked />
 
                           <div className="delivery-info">
@@ -141,13 +149,13 @@ export function CheckOutPage({ cart }) {
                             <p>$9.99 - Shipping</p>
                           </div>
                         </label> */}
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
 
           {/* <div className="product-image-cotent">
             <div className="product-image">
@@ -372,43 +380,47 @@ export function CheckOutPage({ cart }) {
         <div className="order-summary">
           <h3 className="summary-title"> Order Summary </h3>
 
-          <div>
-            <p>Item (0):</p>
+          {paymentSummary && (
+            <>
+              <div>
+                <p>Item ({paymentSummary.totalItems}):</p>
 
-            <p>$0.00</p>
-          </div>
-          <div>
-            <p>Shipping and Handling :</p>
+                <p>{formatCurrency(paymentSummary.productCostCents)}</p>
+              </div>
 
-            <p>$0.00</p>
-          </div>
+              <div>
+                <p>Shipping and Handling :</p>
 
-          <hr />
+                <p>{formatCurrency(paymentSummary.shippingCostCents)}</p>
+              </div>
 
-          <div>
-            <p>Estimated tax (10%):</p>
+              <hr />
 
-            <p>$0.00</p>
-          </div>
+              <div>
+                <p>Estimated tax (10%):</p>
 
-          <div>
-            <p>Total before tax:</p>
+                <p>{formatCurrency(paymentSummary.taxCents)}</p>
+              </div>
 
-            <p>$0.00</p>
-          </div>
+              <div>
+                <p>Total before tax:</p>
 
-          <hr />
+                <p>{formatCurrency(paymentSummary.totalCostBeforeTaxCents)}</p>
+              </div>
 
-          <div className="Total">
-            <h3>Order Total</h3>
+              <hr />
 
-            <h3>$0.00</h3>
-          </div>
+              <div className="Total">
+                <h3>Order Total</h3>
 
-          <a className="order-btn call-to-action" href="./order">
-            {" "}
-            Place Order
-          </a>
+                <h3>{formatCurrency(paymentSummary.totalCostCents)}</h3>
+              </div>
+
+              <a className="order-btn call-to-action" href="./order">
+                Place Order
+              </a>
+            </>
+          )}
         </div>
       </div>
     </>
