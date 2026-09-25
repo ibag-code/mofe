99d30cf26d53f7f0f4902549ea4fd0f19dc33e87
ibag <ibagdesignengineer@gmail.com>
2026-09-25 09:56:48 +0100

    make the Homepage logo visible

--- Mofe-commerce/src/components/Header.jsx
+++ Mofe-commerce/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router'
+import logo from '../assets/logo.jpg'
 
 export  function Checkoutheader () {
     return (
@@ -7,7 +8,7 @@ export  function Checkoutheader () {
 
                 <Link className="vurno-logo" to='/'>
 
-                    <img src="/product/Vurno-logo.jpg" className="Vurno-logo" />
+                    <img src={logo} className="Vurno-logo" />
                     
                 </Link>
 
